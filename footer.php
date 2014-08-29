5e7c73b0ba83e611d253aff58398bb37f5c5b095
Jacob Arriola <jacob.arriola@gmail.com>
2014-08-29 10:26:26 -0700

    Adding Sass files

---
+++ footer.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Floating Orchard
+ */
+?>
+
+      <!-- begin Footer -->
+      <footer>
+          <div class="row section footer-first-section">
+            <div class="large-3 columns">
+              <h5>Contact</h5>
+              <p><a href="mailto:sales@floatingorchard.com">sales@floatingorchard.com</a><br>
+                (888) 970-8870</p>
+            </div> 
+
+            <div class="large-3 columns">
+              <h5>Address</h5>
+              <p>236-250 Greenpoint Ave<br>
+                Box 12<br>
+                Brooklyn, NY 11222</p>
+            </div> 
+
+            <div class="large-6 columns">
+              <h5>Disclaimer</h5>
+              <p class="disclaimer">The Casper Suite is a trademark of JAMF Software.  Mac, iOS, iPhone and iPad are trademarks of Apple Inc.  All other brands and trademarks are the property of their respective owners.</p>
+            </div> 
+          </div> 
+
+          <div class="row section footer-second-section">
+            <div class="medium-8 columns">
+              <p>&copy; <?php echo date("Y") ?> Floating Orchard Inc.
+                New York, NY.  All Rights Reserved</p>
+            </div>
+            <!--
+            <div class="medium-4 columns medium-text-right">
+              <p><strong>Contact | Terms of Service</strong></p>
+            </div>
+          </div> -->
+      </footer><!-- end Footer --> 
+
+    
+    <script src="<?php bloginfo('template_directory'); ?>/js/vendor/jquery.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/foundation.min.js"></script>
+    
+    <script>
+      $(document).foundation();
+    </script>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
+
+    
+
---
+++ front-page.php
@@ -0,0 +1,143 @@
+ <?php get_header(); ?>
+      <!--begin Splash -->
+      <div class="row splash section" data-interchange="[<?php bloginfo('template_directory'); ?>/img/space.png, (small)], [<?php bloginfo('template_directory'); ?>/img/splash.png, (medium)]">
+        <div class="large-12 columns">
+          <div class="row">
+            <div class="large-6 columns end">
+              <h1>Floating Orchard</h1>
+            </div> 
+          </div>
+          <div class="row">
+            <div class="medium-7 large-5 columns end">
+              <p><strong>A quality of business service for the Apple platform.</strong>  Floating Orchard Cloud Services help firms of every size use their Apple devices. We automate forward-thinking solutions so you can focus on productivity. For Mac®, iPhone® and iPad®. </p> 
+              <a href="#contact-form"><button>Free Trial</button></a>
+            </div>
+          </div>
+        </div>
+      </div><!--end Splash -->
+
+      <!-- begin Core Commitments -->
+        <div class="row text-center core-commitments section">
+          <div class="column">
+            <div class="row commitment-content">
+              <div class="medium-8 large-7 medium-centered columns">
+                <h2>Core Commitments</h2>
+                <p>Floating Orchard Cloud Services ensure your Apple desktops, laptops and mobile devices meet IT best practices. That’s not just good business, it's peace of mind.</p>
+              </div>
+            </div>
+
+            <!-- begin first row -->
+            <div class="row text-center orchard-features first-row-icons">
+              <div class="medium-4 columns">
+                <div class="row icon-row">
+                  <div class="small-6 large-7 small-centered columns">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/monitoring@2x.png" alt="monitoring icon">
+                  </div>
+                </div>
+                <h3>Monitoring</h3>
+                <p>Floating Orchard tracks the vital signs your Macs and iOS devices. Helpful for daily troubleshooting, essential for the occasional crisis.</p>
+              </div>
+
+              <div class="medium-4 columns">
+                <div class="row icon-row">
+                  <div class="small-6 large-7 small-centered columns">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/protection@2x.png" alt="protection icon">
+                  </div>
+                </div>
+                <h3>Protection</h3>
+                <p>Less threats &#8800; No threats. Floating Orchard protects your users from emerging malware threats.</p>
+              </div>
+
+              <div class="medium-4 columns">
+                <div class="row icon-row">
+                  <div class="small-6 large-7 small-centered columns">
+                    <img src="<?php bloginfo('template_directory'); ?>/img/updates@2x.png">
+                  </div>
+                </div>
+                <h3>Updates</h3>
+                <p>Updates are inevitable.  Floating Orchard automates them, giving you more time to focus on everything else.</p>
+              </div>
+            </div><!-- end first row -->
+
+            <!-- begin second row -->
+            <div class="row text-center orchard-features">
+              <div class="medium-4 medium-offset-2 columns">
+                <div class="row">
+                  <div class="small-6 large-7 small-centered columns">
+                    <div class="icon-row">
+                      <img src="<?php bloginfo('template_directory'); ?>/img/backup@2x.png" class="home-icons">
+                    </div>  
+                  </div>
+                </div>
+                <h3>Backup</h3>
+                <p>Dealing with your own backups can be a chore.  Dealing with everyone's backups is a breeze with Floating Orchard.</p>
+              </div>
+
+              <div class="medium-4 columns end">
+                <div class="row">
+                  <div class="small-6 large-7 small-centered columns">
+                    <div class="icon-row">
+                      <img src="<?php bloginfo('template_directory'); ?>/img/security@2x.png" class="home-icons">
+                    </div>
+                  </div>
+                </div>
+                  <h3>Security</h3>
+                  <p>Every firm's data is its lifeblood.  Floating Orchard helps you prevent loss with encryption and network security monitoring. Your IP stays safe inside, intruders stay out.</p>
+              </div>
+            </div><!--end second row-->
+          </div> 
+        </div> <!-- end Core Commitments -->
+         
+      <!-- begin Our Customers -->
+      <div class="row our-customers section" data-interchange="[<?php bloginfo('template_directory'); ?>/img/space.png, (small)], [<?php bloginfo('template_directory'); ?>/img/customer.png, (medium)]">
+        <div class="medium-4 columns">
+          <h2>Our Customers</h2>
+          <p>Floating Orchard Cloud Services and Solutions empower your Apple management needs, whether you are the Office Techie or a whole Tech Department. We help you streamline your endpoints so everyone stays on the same page with less effort.</p>
+        </div>
+      </div><!-- end Our Customers -->
+
+      <!-- begin Cloud Technologies -->
+      <div class="row cloud-technologies section">
+        <div class="large-12 columns text-center">
+         <img src="<?php bloginfo('template_directory'); ?>/img/devices.png" alt="Image of devices" />
+        </div>
+        <div class="medium-8 large-6 medium-centered columns text-center">
+          <h2>Cloud Technologies</h2>
+          <p>Floating Orchard follows you and your users anywhere on the planet because the mobile workforce is the new offce. <br> <strong>Powered by the Casper Suite.</strong></p>
+        </div>
+      </div><!-- end Cloud Technologies -->
+
+      <!-- begin Partnerships -->
+      <div class="partnerships row section">
+        <div class="large-12 columns">
+          <div class="row">
+            <div class="medium-8 large-5 medium-centered columns text-center">
+              <h2>Partnerships</h2>
+              <p>Excellent requires Teamwork.</p>
+            </div>
+          </div>
+  
+          <div class="row partnership-logos">
+            <div class="small-5 medium-4 large-3 small-offset-1 medium-offset-2 large-offset-3 text-center columns">
+              <img src="<?php bloginfo('template_directory'); ?>/img/casper.jpg" alt="Casper Suite logo" title="Casper Suite logo" class="casper-image" />
+            </div>
+            <div class="small-5 medium-4 large-3 columns text-center end">
+              <img src="<?php bloginfo('template_directory'); ?>/img/apple.png" alt="Apple Consultants Network logo" title="Apple Consultants Network logo" class="consultants-network-image" />
+            </div>
+          </div>
+        </div>
+      </div><!-- end Partnerships -->
+
+      <!-- begin Contact Area -->
+      <div class="row contact-area section" id="contact-form" data-interchange="[<?php bloginfo('template_directory'); ?>/img/space.png, (default)], [<?php bloginfo('template_directory'); ?>/img/mapcopy.jpg, (medium)]">
+        <!--begin Contact Form -->
+        <div class="columns contact-section">
+          <div class="contact-form">
+            <a name="contactform"></a>
+            <h4>Get a Quote &amp; Free Trial</h4>
+            <p>Fill out the form below to try Floating Orchard.</p>
+            <?php gravity_form(1, false, false, false, '', false); ?>
+          </div><!--end Contact Form -->
+        </div>
+      </div><!-- end Contact Area -->
+<?php get_footer(); ?>
